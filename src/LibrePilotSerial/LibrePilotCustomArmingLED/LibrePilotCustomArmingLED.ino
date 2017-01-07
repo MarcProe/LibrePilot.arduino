7449c29f5aead2de00171895da5dcd98d132c949
Marcus Proest <proemarc@gmail.com>
2017-01-07 12:06:52 +0100

    Initial Import of an example and the library

---
+++ src/LibrePilotSerial/LibrePilotCustomArmingLED/LibrePilotCustomArmingLED.ino
@@ -0,0 +1,48 @@
+#include <SoftwareSerial.h>
+//#include "FlightStatus.h"
+#include "LibrePilotSerial.h"
+
+SoftwareSerial mySerial(2, 3);  // RX, TX
+LibrePilotSerial lps(&mySerial);
+
+int ledPinr = 9;
+int ledPiny = 8;
+int ledPing = 7;
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(57600);
+  //initialize LibrePilotSerial Object
+  lps.serial->begin(57600);
+}
+
+void loop() {
+  
+  lps.request(FLIGHTSTATUS_OBJID);
+  boolean ok = lps.receive(FLIGHTSTATUS_OBJID, FlightStatusDataUnion.arr, 200);
+
+  Serial.print(" Result fs: ");
+  Serial.println(FlightStatusDataUnion.data.Armed);      
+
+  if(ok) {
+    if(FlightStatusDataUnion.data.Armed == FLIGHTSTATUS_ARMED_DISARMED) {
+      analogWrite(ledPinr, 0);
+      analogWrite(ledPiny, 0);
+      analogWrite(ledPing, 255);
+    } else if (FlightStatusDataUnion.data.Armed == FLIGHTSTATUS_ARMED_ARMING) {
+      analogWrite(ledPinr, 0);
+      analogWrite(ledPiny, 255);
+      analogWrite(ledPing, 0);
+    } else if (FlightStatusDataUnion.data.Armed == FLIGHTSTATUS_ARMED_ARMED) {
+      analogWrite(ledPinr, 255);
+      analogWrite(ledPiny, 0);
+      analogWrite(ledPing, 0);
+    }
+  }
+
+  delay(50);
+}
+
+
+
+
---
+++ src/LibrePilotSerial/LibrePilotSerial/LibrePilotSerial.h
@@ -0,0 +1,26 @@
+#ifndef LibrePilotSerial_h
+#define LibrePilotSerial_h
+
+#include "Arduino.h"
+#include <SoftwareSerial.h>
+
+#define byte uint8_t
+
+union {
+    float f;
+    unsigned long ul;
+ } u2f;
+
+class LibrePilotSerial {
+  public:
+    LibrePilotSerial(SoftwareSerial* ser);
+    boolean receive(unsigned long objId, byte *ret, unsigned int timeout = 100);
+    void request(unsigned long objId);
+    SoftwareSerial* serial;
+    
+  private:
+    byte _pBuf[265];
+    byte _crc(int len);
+};
+
+#endif
